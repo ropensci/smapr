91672e7c5571f317ec9e6da5db1f4fe97681ed1c
Max Joseph <maxwellbjoseph@gmail.com>
2016-05-19 12:27:39 -0600

    adding tests for invalid datasets and input files for extract_smap

--- tests/testthat/test-extract_smap.R
+++ tests/testthat/test-extract_smap.R
@@ -8,3 +8,21 @@ test_that("invalid groups cause errors", {
                               group = 'Non-existent group',
                               dataset = 'leaf_area_index'))
 })
+
+test_that("invalid datasets cause errors", {
+    files <- find_smap(id = "SPL4SMGP", date = "2015.03.31")
+    downloads <- download_smap(files[1, ])
+    h5_data <- subset(downloads, file_ext == '.h5')
+    expect_error(extract_smap(h5_data$file[1],
+                              group = 'Geophysical_Data',
+                              dataset = 'Nonexistent_dataset'))
+})
+
+test_that("non-hdf5 input files cause errors", {
+    files <- find_smap(id = "SPL4SMGP", date = "2015.03.31")
+    downloads <- download_smap(files[1, ])
+    h5_data <- subset(downloads, file_ext == '.qa')
+    expect_error(extract_smap(h5_data$file[1],
+                              group = 'Geophysical_Data',
+                              dataset = 'leaf_area_index'))
+})
