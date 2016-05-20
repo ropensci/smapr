99952715cb2ce7767ce347ac777a1d21e86d4868
Max Joseph <maxwellbjoseph@gmail.com>
2016-05-20 13:29:33 -0600

    factoring rasterization of smap data & computation of the grid extent

--- R/extract_smap.R
+++ R/extract_smap.R
@@ -10,7 +10,7 @@
 #' @param data A data frame produced by \code{download_smap()} that specifies
 #' input files from which to extract data.
 #' @param name The path in the HDF5 file pointing to data to extract.
-#' @param in_memory Logical. Should the result be stored in memory
+#' @param in_memory Logical. Should the result be stored in memory?
 #' (\code{in_memory = TRUE}) or on disk (\code{in_memory = FALSE}). By default
 #' the result is stored on disk.
 #' @return Returns a Raster object.
@@ -28,26 +28,33 @@
 extract_smap <- function(data = NULL, name, in_memory = FALSE) {
     h5_files <- data[data$extension == ".h5", 'local_file']
     n_files <- length(h5_files)
-
-    raster_list <- vector("list", length = n_files)
+    rasters <- vector("list", length = n_files)
     for (i in 1:n_files) {
-        h5_in <- h5read(h5_files[i], name)
-        h5_in[h5_in == -9999] <- NA
-        r <- raster(t(h5_in))
-        raster::extent(r) <- compute_extent(h5_files[i])
-        raster::projection(r) <- smap_crs()
-        raster_list[[i]] <- r
+        rasters[[i]] <- rasterize_smap(h5_files[i], name)
     }
-    make_stack(raster_list, in_memory)
+    make_stack(rasters, in_memory)
+}
+
+rasterize_smap <- function(file, name) {
+    h5_in <- h5read(file, name)
+    h5_in[h5_in == -9999] <- NA
+    r <- raster(t(h5_in))
+    raster::extent(r) <- compute_extent(file)
+    raster::projection(r) <- smap_crs()
+    r
 }
 
 compute_extent <- function(h5_file) {
-    lon <- h5read(h5_file, '/cell_lon')[, 1]
-    lat <- h5read(h5_file, '/cell_lat')[1, ]
-    grid_extent <- raster::extent(range(lon), range(lat))
+    grid_extent <- compute_grid_extent(h5_file)
     grid_raster <- raster(grid_extent, crs = latlon_crs())
     projected_extent <- projectExtent(grid_raster, smap_crs())
     raster::extent(projected_extent)
+}
+
+compute_grid_extent <- function(h5_file) {
+    lon <- h5read(h5_file, '/cell_lon')[, 1]
+    lat <- h5read(h5_file, '/cell_lat')[1, ]
+    raster::extent(range(lon), range(lat))
 }
 
 make_stack <- function(r_list, in_memory) {
