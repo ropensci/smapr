a233487772f02c91115e934d8f37f7d8f1dda7ff
Max Joseph <maxwellbjoseph@gmail.com>
2016-05-26 15:58:13 -0600

    adding a test to verify output classes for list_smap

--- tests/testthat/test-list_smap.R
+++ tests/testthat/test-list_smap.R
@@ -2,6 +2,14 @@ context("list_smap")
 
 test_that("vector input causes errors", {
     files <- find_smap(id = "SPL4SMGP", date = "2015.03.31", version = 1)
-    files <- download_smap(files[1, ])
-    expect_error(list_smap(files$local_file))
+    downloads <- download_smap(files[1, ])
+    expect_error(list_smap(downloads$local_file))
+})
+
+test_that("list_smap returns a list of data frames", {
+    files <- find_smap(id = "SPL4SMGP", date = "2015.03.31", version = 1)
+    downloads <- download_smap(files[1, ])
+    contents <- list_smap(downloads)
+    expect_that(contents, is_a("list"))
+    expect_that(contents[[1]], is_a("data.frame"))
 })
