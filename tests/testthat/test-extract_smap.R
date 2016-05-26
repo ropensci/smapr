f50e567775d614921cc600613677e808e62a9059
Max Joseph <maxwellbjoseph@gmail.com>
2016-05-26 15:52:07 -0600

    adding a test to verify the class of the output of extract_smap

--- tests/testthat/test-extract_smap.R
+++ tests/testthat/test-extract_smap.R
@@ -7,3 +7,12 @@ test_that("invalid datasets cause errors", {
                               name = '/Geophysical_Data/soil_flavor',
                               in_memory = TRUE))
 })
+
+test_that("extract_smap produces a RasterStack", {
+    files <- find_smap(id = "SPL4SMGP", date = "2015.03.31", version = 1)
+    downloads <- download_smap(files[1, ])
+    r <- extract_smap(downloads,
+                      name = '/Geophysical_Data/sm_surface',
+                      in_memory = TRUE)
+    expect_that(r, is_a("RasterStack"))
+})
