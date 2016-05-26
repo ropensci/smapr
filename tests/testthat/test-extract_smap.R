1c65bdd0af3940a9475504d99a858fce1c0d8d2d
Max Joseph <maxwellbjoseph@gmail.com>
2016-05-26 15:49:37 -0600

    fixing test for invalid datasets. Previously we were passing the test for the wrong reason (we received an error because we used the wrong function arguments)

--- tests/testthat/test-extract_smap.R
+++ tests/testthat/test-extract_smap.R
@@ -1,19 +1,9 @@
 context("extract_smap")
 
-test_that("invalid groups cause errors", {
-    files <- find_smap(id = "SPL4SMGP", date = "2015.03.31", version = 1)
-    downloads <- download_smap(files[1, ])
-    expect_error(extract_smap(downloads,
-                              group = 'Non-existent group',
-                              dataset = 'leaf_area_index'))
-})
-
 test_that("invalid datasets cause errors", {
     files <- find_smap(id = "SPL4SMGP", date = "2015.03.31", version = 1)
     downloads <- download_smap(files[1, ])
     expect_error(extract_smap(downloads,
-                              group = 'Geophysical_Data',
-                              dataset = 'Nonexistent_dataset'))
+                              name = '/Geophysical_Data/soil_flavor',
+                              in_memory = TRUE))
 })
-
-
