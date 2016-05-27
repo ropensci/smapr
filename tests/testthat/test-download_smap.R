bbf8d05337fd1af018a3ca92e67822afdb44116a
Max Joseph <maxwellbjoseph@gmail.com>
2016-05-27 10:08:05 -0600

    adding some tests and removing an old test that didn't really test anything

--- tests/testthat/test-download_smap.R
+++ tests/testthat/test-download_smap.R
@@ -5,7 +5,11 @@ test_that("invalid output directories cause errors", {
     expect_error(download_smap(files[1, ], dir = 1234))
 })
 
-test_that("non-existant output directories do not cause errors", {
-    files <- find_smap(id = "SPL4SMGP", date = "2015.03.31", version = 1)
-    expect_error(download_smap(files[1, ], dir = NULL), NA)
+test_that("non-existent directories are created", {
+    files <- find_smap(id = "SPL3SMP", date = "2015.05.01", version = 2)
+    dir_name <- "silly_nonexistent_directory"
+    downloads <- download_smap(files, directory = dir_name)
+    expect_true(dir.exists(dir_name))
+    # cleanup by removing directory
+    unlink(dir_name, recursive = TRUE)
 })
--- tests/testthat/test-extract_smap.R
+++ tests/testthat/test-extract_smap.R
@@ -16,3 +16,13 @@ test_that("extract_smap produces a RasterStack", {
                       in_memory = TRUE)
     expect_that(r, is_a("RasterStack"))
 })
+
+test_that("-9999 is used fill value when a _FillValue doesn't exist", {
+    files <- find_smap(id = "SPL3SMP", date = "2015.05.01", version = 2)
+    downloads <- download_smap(files)
+    r <- extract_smap(downloads, name = "Soil_Moisture_Retrieval_Data/latitude")
+    # the fill value in the file is -9999, but there is no fill value attribute
+    # therefore, if this function works, the minimum should be >= -90
+    # (the latitude at the south pole)
+    expect_gte(raster::minValue(r), -90)
+})
