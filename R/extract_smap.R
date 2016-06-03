eebae2aee6ccf20d5fbbe9bc12079f03e8e497a9
Max Joseph <maxwellbjoseph@gmail.com>
2016-06-03 12:32:05 -0600

    adding support for the projection and extent of SPL3FT, but we still need to do something about the ascending and descending tracks...

--- R/extract_smap.R
+++ R/extract_smap.R
@@ -40,6 +40,9 @@ extract_smap <- function(data, name, in_memory = FALSE) {
 
 rasterize_smap <- function(file, name) {
     h5_in <- h5read(file, name)
+    if (length(dim(h5_in)) > 2) {
+        h5_in <- h5_in[, , 1]
+    }
     fill_value <- find_fill_value(file, name)
     h5_in[h5_in == fill_value] <- NA
     raster_layer <- raster(t(h5_in))
@@ -60,26 +63,40 @@ find_fill_value <- function(file, name) {
 
 project_smap <- function(file, smap_raster) {
     raster::extent(smap_raster) <- compute_extent(file)
-    raster::projection(smap_raster) <- smap_crs()
+    raster::projection(smap_raster) <- smap_crs(file)
     smap_raster
 }
 
 compute_extent <- function(h5_file) {
     latlon_extent <- compute_latlon_extent(h5_file)
     latlon_raster <- raster(latlon_extent, crs = latlon_crs())
-    projected_extent <- projectExtent(latlon_raster, smap_crs())
-    smap_extent <- raster::extent(projected_extent)
+    pr_extent <- projectExtent(latlon_raster, smap_crs(h5_file))
+    if (is_L3FT(h5_file)) {
+        raster::extent(pr_extent)[3] <- -raster::extent(pr_extent)[4]
+        smap_extent <- raster::extent(pr_extent)
+    } else {
+        smap_extent <- raster::extent(pr_extent)
+    }
     smap_extent
 }
 
 compute_latlon_extent <- function(h5_file) {
-    extent_list <- h5readAttributes(h5_file, "Metadata/Extent")
-    extent_vector <- with(extent_list, {
-        c(westBoundLongitude, eastBoundLongitude,
-          southBoundLatitude, northBoundLatitude)
-    })
+    if (is_L3FT(h5_file)) {
+        # b/c metadata are incorrect in L3_FT data files
+        extent_vector <- c(-180, 180, 0, 90)
+    } else {
+        extent_list <- h5readAttributes(h5_file, "Metadata/Extent")
+        extent_vector <- with(extent_list, {
+            c(westBoundLongitude, eastBoundLongitude,
+              southBoundLatitude, northBoundLatitude)
+        })
+    }
     latlon_extent <- raster::extent(extent_vector)
     latlon_extent
+}
+
+is_L3FT <- function(filename) {
+    grepl("L3_FT", filename)
 }
 
 make_stack <- function(r_list, in_memory) {
--- R/zzz.R
+++ R/zzz.R
@@ -10,8 +10,13 @@ min_extensions <- function(){
     c('.h5', '.h5.iso.xml')
 }
 
-smap_crs <- function() {
-    "+proj=cea +lat_ts=30 +datum=WGS84 +units=m"
+smap_crs <- function(file) {
+    if (is_L3FT(file)) {
+        crs <- "+proj=laea +lon_0=0 +lat_0=90 +datum=WGS84 +units=m"
+    } else {
+        crs <- "+proj=cea +lat_ts=30 +datum=WGS84 +units=m"
+    }
+    crs
 }
 
 latlon_crs <- function() {
