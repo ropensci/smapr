aba9d64d211851352de56c4a3d7c701fb927f787
Max Joseph <maxwellbjoseph@gmail.com>
2016-06-06 16:00:48 -0600

    using smaller SPL3SMP data for download_smap tests

--- tests/testthat/test-download_smap.R
+++ tests/testthat/test-download_smap.R
@@ -1,12 +1,12 @@
 context("download_smap")
 
 test_that("invalid output directories cause errors", {
-    files <- find_smap(id = "SPL4SMGP", date = "2015.03.31", version = 1)
+    files <- find_smap(id = "SPL3SMP", date = "2015.03.31", version = 3)
     expect_error(download_smap(files[1, ], dir = 1234))
 })
 
 test_that("non-existent directories are created", {
-    files <- find_smap(id = "SPL3SMP", date = "2015.05.01", version = 2)
+    files <- find_smap(id = "SPL3SMP", date = "2015.03.31", version = 3)
     dir_name <- "silly_nonexistent_directory"
     downloads <- download_smap(files, directory = dir_name)
     expect_true(dir.exists(dir_name))
@@ -15,7 +15,7 @@ test_that("non-existent directories are created", {
 })
 
 test_that("the downloaded data is of the data frame class", {
-    files <- find_smap(id = "SPL3SMP", date = "2015.05.01", version = 2)
+    files <- find_smap(id = "SPL3SMP", date = "2015.03.31", version = 3)
     downloads <- download_smap(files[1, ])
     expect_that(downloads, is_a("data.frame"))
 })
