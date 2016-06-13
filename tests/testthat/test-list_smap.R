79600b2ae3d6c286cc78661735e9209b86db886a
Max Joseph <maxwellbjoseph@gmail.com>
2016-06-13 15:26:26 -0600

    using smaller data sets for the testing of list_smap

--- tests/testthat/test-list_smap.R
+++ tests/testthat/test-list_smap.R
@@ -1,13 +1,13 @@
 context("list_smap")
 
 test_that("vector input causes errors", {
-    files <- find_smap(id = "SPL4SMGP", date = "2015.03.31", version = 1)
-    files <- download_smap(files[1, ])
-    expect_error(list_smap(files$local_file))
+    files <- find_smap(id = "SPL3SMP", date = "2015.05.01", version = 2)
+    downloads <- download_smap(files[1, ])
+    expect_error(list_smap(downloads$local_file))
 })
 
 test_that("list_smap returns a list of data frames", {
-    files <- find_smap(id = "SPL4SMGP", date = "2015.03.31", version = 1)
+    files <- find_smap(id = "SPL3SMP", date = "2015.05.01", version = 2)
     downloads <- download_smap(files[1, ])
     contents <- list_smap(downloads)
     expect_that(contents, is_a("list"))
@@ -18,9 +18,7 @@ test_that("list_smap returns a data frame with the proper column names", {
     files <- find_smap(id = "SPL3SMP", date = "2015.05.01", version = 2)
     downloads <- download_smap(files[1, ])
     contents <- list_smap(downloads)
-    expect_that(colnames(contents[[1]][1]), matches("group"))
-    expect_that(colnames(contents[[1]][2]), matches("name"))
-    expect_that(colnames(contents[[1]][3]), matches("otype"))
-    expect_that(colnames(contents[[1]][4]), matches("dclass"))
-    expect_that(colnames(contents[[1]][5]), matches("dim"))
+    df_names <- names(contents[[1]])
+    expected_names <- c('group', 'name', 'otype', 'dclass', 'dim')
+    expect_equal(df_names, expected_names)
 })
